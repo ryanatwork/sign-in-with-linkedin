c0085ac80db747d140c896eca1726b5e9e9d2f64
Ryan Resella <ryan@codeforamerica.org>
2011-09-04 15:58:10 -0700

    Implemented sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,26 @@
 class SessionsController < ApplicationController
-  def show
+  def create
+    session[:access_token] = request.env['omniauth.auth']['credentials']['token']
+    session[:access_secret] = request.env['omniauth.auth']['credentials']['secret']
+    redirect_to show_path, :notice => "Signed in with LinkedIn!"
   end
 
+  def show
+    if session['access_token'] && session['access_secret']
+      client.authorize_from_access(session['access_token'], session['access_secret'])
+      @user = client.profile
+    else
+      redirect_to failure_path
+    end
+  end
+
+  def error
+    flash[:error] = "Sign in with LinkedIn failed!"
+    redirect_to root_path
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, :notice => "Signed out!"
+  end
 end
