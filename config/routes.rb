e153a907cdd6d524f68884af06b9236060450a65
Ryan Resella <ryan@codeforamerica.org>
2011-09-04 15:02:29 -0700

    Updated routes for oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SignInWithLinkedin::Application.routes.draw do
   root :to => "welcome#index"
-
+  get '/auth/linked_in/callback', :to => 'sessions#create', :as => 'callback'
+  get '/auth/failure', :to => 'sessions#error', :as => 'failure'
+  get '/profile', :to => 'sessions#show', :as => 'show'
+  delete '/signout', :to => 'sessions#destroy', :as => 'signout'
 end
