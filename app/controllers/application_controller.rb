76e8f4cc8a322f4ccce7baa90e466affd7e07609
Ryan Resella <ryan@codeforamerica.org>
2011-09-04 15:12:19 -0700

    Added LinkedIn configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def client
+    LinkedIn.configure do |config|
+      config.token = ENV['CONSUMER_KEY']
+      config.secret = ENV['CONSUMER_SECRET']
+      config.default_profile_fields = ['certifications','date-of-birth','educations',
+        'phone-numbers','positions','picture-url','skills','summary']
+    end
+
+    @client ||= LinkedIn::Client.new
+    @client.authorize_from_access(session['access_token'], session['access_secret'])
+  end
+
 end
