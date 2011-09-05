a519335ed6bffda92589fb9dff42429067f547df
Ryan Resella <ryan@codeforamerica.org>
2011-09-04 23:00:27 -0700

    Added connections to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SignInWithLinkedin::Application.routes.draw do
-  get "connections/show"
 
   root :to => "welcome#index"
   get '/auth/linked_in/callback', :to => 'sessions#create', :as => 'callback'
   get '/auth/failure', :to => 'sessions#error', :as => 'failure'
+  get '/connections', :to => 'connections#show', :as => 'show'
   get '/profile', :to => 'sessions#show', :as => 'show'
   delete '/signout', :to => 'sessions#destroy', :as => 'signout'
 end
